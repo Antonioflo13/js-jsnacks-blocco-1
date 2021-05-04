6d9e33b15b26a347dc4949f3299a6c6c473d897a
Antonioflo13 <floreantonio@hotmail.it>
2021-05-04 17:01:17 +0200

    fix

--- snack4/js/script.js
+++ snack4/js/script.js
@@ -2,13 +2,13 @@
 // Generatore di “nomi cognomi” casuali: scrivere una funzione che, partendo da una lista di nomi e una lista di cognomi, generi una coppia nome-cognome casuale, così da permettere al signor Gatsby di costruire una falsa lista di 10 invitati alla sua festa, da stampare in pagina, utilizzando il tag adatto alle liste.
 
 var listaCognomi = ["Lococciolo", "Rossi", "Lombardi"] ;
-console.log(listaCognomi);
+// console.log(listaCognomi);
 var listaNomi = ["Antonio", "Claudia", "Francesca"] ;
-console.log(listaNomi);
+// console.log(listaNomi);
 
 function nomeCasuale (nome, cognome) {
     var nomeCasuale = Math.floor(Math.random()* nome.length);
-    console.log("nome casuale " + nomeCasuale);
+    // console.log("nome casuale " + nomeCasuale);
     var cognomeCasuale = Math.floor(Math.random()* cognome.length);
     
     return nome[nomeCasuale] + " " + cognome[cognomeCasuale];
